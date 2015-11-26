1707cbaba744adef5cc49a6d15945544dd4ff77c
Richard Kipsang (alias Richard Keep) <r.kipsang@gmail.com>
2015-11-27 00:10:55 +0300

    remove closure

--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,3 @@
 <?php
 
-Route::post('validate', function(Illuminate\Http\Request $request){
-
-	$field = preg_replace('/-/', '_', $request->get('field'));
-	
-	$data = [
-		"{$field}" => $request->get('data')
-	];
-
-	$v = Validator::make($data, config('richard.rules'));
-
-	return json_encode( $v->errors()->first( $field ) );
-	
-});
+Route::post('validate', 'RichardKeep\Validate\ValidationController@validate');
