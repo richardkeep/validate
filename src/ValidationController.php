1463b792e26e48b1a21142f00fe8e402f7ff9e46
Richard Kipsang (alias Richard Keep) <r.kipsang@gmail.com>
2015-11-27 00:12:33 +0300

    Create ValidationController.php

---
+++ src/ValidationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RichardKeep\Validate;
+
+use Validator;
+use Illuminate\Http\Request;
+
+class ValidationController
+{
+        // Validate a POST request
+        
+        public function validate(Request $request)
+        {
+
+	        $field = preg_replace('/-/', '_', $request->get('field'));
+	
+        	$data = [
+        		"{$field}" => $request->get('data')
+        	];
+
+        	$v = Validator::make($data, config('richard.rules'));
+        
+        	return json_encode( $v->errors()->first( $field ) );  
+        }
+}
